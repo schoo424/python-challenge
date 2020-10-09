35e02a23680d1a81d0e4d8f8d5fc848f4ed7dff0
schoo424 <Sung.Choo@nike.com>
2020-10-09 10:38:10 -0700

    add my homework

--- PyBank/main.py
+++ PyBank/main.py
@@ -16,19 +16,41 @@ with open(csvpath) as csvfile:
 
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    # print(f"CSV Header: {csv_header}")
     
     
-    # Read each row of data after the header, add index
-    index = 0
+    # index = 0
     for row in csvreader:
+                
+        row_count = len(list(csvreader)) + 1
         
-        #print(row)
-        #print(f'[{str(csvreader.index(row))}] {row}')
+        print("Financial Analysis")
+        print("---------------------------------------------------------------------------------")
+        print("Total Month: " + str(row_count))
+
+        
+        
+# get the row count in the CSV 
+        # row_count = len(list(csvreader)
+        # record_count = row_count -1
+        # print(record_count
+
+        # def row_count()
         # print(f"[{index}] {row}")
-        # index += 1
-        print(index, row)
-        index += 1
-
+        # index += 1        
 #total number of months included in the dataset
+#   The net total amount of "Profit/Losses" over the entire period
 
+#   The average of the changes in "Profit/Losses" over the entire period
+        # def average(profit_loss):
+        #     length = len(profit_loss)
+        #     total = 0.0
+        #     for number in profit_loss:
+        #             total += number
+        #     return total / length
+        
+        
+#   The greatest increase in profits (date and amount) over the entire period
+
+#   The greatest decrease in losses (date and amount) over the entire period
+    
