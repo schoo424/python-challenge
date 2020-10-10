53e7e3dde76ca2581e32e65171802bc8498e713b
schoo424 <Sung.Choo@nike.com>
2020-10-10 10:02:21 -0700

    add my PyPoll HW

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,9 +7,13 @@ csvpath = os.path.join('Resources', 'election_data.csv')
 with open(csvpath, 'r') as csvfile:
 
     total_vote = 0
-
+    election_data = {}
     for row in csv.DictReader(csvfile):
         total_vote += 1
 
+   
+
 print ("Total Vote: " + str(total_vote))
-print(f'{len(row[2]}')
+
+
+
