f48e2aa471c3304e146e2ecb11f231addbb40e42
schoo424 <Sung.Choo@nike.com>
2020-10-11 07:35:12 -0700

    add my PyPoll homework

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,13 +7,20 @@ csvpath = os.path.join('Resources', 'election_data.csv')
 with open(csvpath, 'r') as csvfile:
 
     total_vote = 0
-    election_data = {}
+    candidate_totals = dict()
+
+    # all_candidate_with_duplicates = []
+    # all_candidate_with_duplicates.append = row[2]
+    
     for row in csv.DictReader(csvfile):
         total_vote += 1
 
-   
+        # if candidate_totals.get(row[2]):
+        #     candidate_totals += 1
+
+
 
 print ("Total Vote: " + str(total_vote))
-
+print(candidate_totals)
 
 
