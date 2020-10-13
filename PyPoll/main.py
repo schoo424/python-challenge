14de910ba5ec69bd0eeb4f1cf452d7697de05481
schoo424 <Sung.Choo@nike.com>
2020-10-12 17:19:41 -0700

    add my PyPoll homework

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,54 +1,71 @@
+#Modules
 import os
-
 import csv
-
+# define variables
 csvpath = os.path.join('Resources', 'election_data.csv')
-
 with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     print(csvreader)
     csv_header = next(csvreader)
-
-    total_vote = 0
-    candidate_totals = {}
-    candidates_name = []
     
-    # all_candidate_with_duplicates = []
-    # all_candidate_with_duplicates.append = row[2]
-    
-    # for row in election_data.values():
-    #     if row in candidates_list:
-    #         continue
-    #     else:
-    #         candidates_list.append(row)
+total_vote_count = 0
 
 
-    for row in csvfile:
-
-        #count the number of rows without the column headers
-        total_vote +=1
-        candidates_name = row[2]
-        #get the list of candidate names
-        if candidates_name not in candidate_totals:
-            candidate_totals.update({row[2]:1})
-        else:
-            candidate_totals[row[2]] += 1
-        #candidate_totals.append({candidates_name})
-        # if candidate_name in candidate_totals: 
-            
-    
-        # if candidate_name in candidate_totals:
-        #     candidate_totals[candidate_name] = row
-        #     candidate_totals[candidate_name] = candidate_totals
-        # if unique_candidates in candidate_totals:
-        #     candidate_totals[]
+# create null dictionary to store candidate names
+candidatelist = {}
+#Open ElectionData.csv
+with open(csvpath, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+    for row in csvreader:
+        total_vote_count += 1
+        candidate = row[2]
+        if candidate not in candidatelist:
+            #candidatelist.update({candidate : 1})
+            candidatelist[candidate] = candidatelist.get(candidate,0) +1
+        else: 
+            candidatelist[candidate] += 1
         
-            
-        # if candidate_totals.get(row[2]):
-        #     candidate_totals += 1
+        #Percent_Received = candidatelist[1]/total_vote_count
+        
+        #list of candidates
+        
 
+      
+    print("Election Results")     
+    print("-----------------------------------------")
+    print("Total Votes: " + str(total_vote_count))
+    print("-----------------------------------------")
 
+    top_votes = 0
+    winner = ""
 
-print ("Total Vote: " + str(total_vote))
-print("candidate name:" + str(candidate_totals))
+    for key in candidatelist:
+        share_of_votes_received = candidatelist[key] / total_vote_count
+        percent_received = "{:.3%}".format(share_of_votes_received)
+        
+        print(key, ' :', percent_received, candidatelist[key])
+    
+        if candidatelist[key] > top_votes:
+            top_votes = candidatelist[key]
+            winner = key
 
+    print("-----------------------------------------")
+    print("Winner: " + str(winner))
+    
+output_path = os.path.join("Analysis", "PyPoll_HW_Output.csv")
+
+with open(output_path, 'w') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter = ',')
+    csvwriter.writerow(['Election Results'])
+    csvwriter.writerow(['----------------------------------------------------------------------'])
+    csvwriter.writerow(['Total Votes: ' + str(total_vote_count)])
+    csvwriter.writerow(['----------------------------------------------------------------------'])
+    for key in candidatelist:
+        #csvwriter.writerow([key, ' :', percent_received, candidatelist[key]])
+        csvfile.write(f"{key}: {(candidatelist[key]/total_vote_count)*100:.3f}% ({candidatelist[key]})\n")
+    csvwriter.writerow(['----------------------------------------------------------------------'])
+    csvwriter.writerow(['Winner: ', str(winner)])
+    
+    
+   
